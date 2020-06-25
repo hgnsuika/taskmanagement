b40883abffcf474345174f8b0901865935ba230e
shinichiroh nishijima <hgnsuika@gmail.com>
2020-06-25 16:14:25 +0000

    login and one to many

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
     
   def index
-      @tasks = Task.all
+      if logged_in?
+      @task = current_user.tasks.build  
+      @tasks = current_user.tasks
+    
+      
+    end
   end
 
   def show
@@ -14,18 +20,21 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
-
+      @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
+      
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
+  
 
   def edit
+     @task = Task.find(params[:id])
     
   end
 
@@ -52,6 +61,7 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
+  
   
   def set_task
     @task = Task.find(params[:id])
