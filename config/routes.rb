0f33e3b20a0eccf5aa2611dc9e6410858564c3f1
shinichiroh nishijima <hgnsuika@gmail.com>
2020-06-25 05:56:05 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   # get 'toppages/index'
   root to: 'toppages#index'
   # root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   resources :tasks
 
+
 end
 
+
+
+ 
 
